ee2bcbf99497789b558b2dae2daf7905ee55df34
uzzielkyle <uzzielynciong@gmail.com>
2025-09-29 02:28:51 +0800

    feat(robot): add robot class with move and clean methods
    - also applied SRP to other existing methods

--- VacuumCleanerRobot/Program.cs
+++ VacuumCleanerRobot/Program.cs
@@ -5,7 +5,7 @@ namespace VacuumCleanerRobot
     public class Map
     {
         private enum CellType { Empty, Dirt, Obstacle, Cleaned };
-        private CellType[,] _grid;
+        private readonly CellType[,] _grid;
         public int Width { get; private set; }
         public int Height { get; private set; }
 
@@ -30,12 +30,12 @@ namespace VacuumCleanerRobot
 
         public bool IsDirt(int x, int y)
         {
-            return this.IsInBounds(x, y) && this._grid[x, y] == CellType.Dirt;
+            return this._grid[x, y] == CellType.Dirt;
         }
 
         public bool IsObstacle(int x, int y)
         {
-            return this.IsInBounds(x, y) && this._grid[x, y] == CellType.Obstacle;
+            return this._grid[x, y] == CellType.Obstacle;
         }
 
         public void AddDirt(int x, int y)
@@ -87,17 +87,87 @@ namespace VacuumCleanerRobot
         }
     }
 
+    public class Robot(Map map)
+    {
+        private readonly Map _map = map;
+        public int X { get; set; } = 0;
+        public int Y { get; set; } = 0;
+
+        public int SpeedSettings { get; private set; } = 150;
+
+        public void AdjustSpeedSettings(int newSpeedSettings)
+        {
+            if (newSpeedSettings <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(newSpeedSettings),
+                    "Speed settings must be greater than zero."
+                );
+            }
+
+            this.SpeedSettings = newSpeedSettings;
+        }
+
+        public bool Move(int newX, int newY)
+        {
+            if (this._map.IsObstacle(newX, newY) || !this._map.IsInBounds(newX, newY))
+                return false;
+
+            // set new location
+            this.X = newX;
+            this.Y = newY;
+
+            // display the map with the robot's new location
+            this._map.Display(this.X, this.Y);
+            return true;
+        }
+
+        public void CleanCurrentSpot()
+        {
+            if (!this._map.IsDirt(this.X, this.Y)) return;
+
+            this._map.Clean(this.X, this.Y);
+            this._map.Display(this.X, this.Y);
+        }
+
+        public void StartCleaning()
+        {
+            Console.WriteLine("Start cleaning the room");
+            // flag that determines the direction
+            int direction = 1;
+            for (int x = 0; x < this._map.Width; x++)
+            {
+                int startY = (direction == 1) ? 0 : this._map.Height - 1;
+                int endY = (direction == 1) ? this._map.Height : -1;
+
+                for (int y = startY; y != endY; y += direction)
+                {
+                    if (!Move(x, y))
+                    {
+                        // obstacle detected
+                    }
+                    CleanCurrentSpot();
+                    Thread.Sleep(150);
+                }
+                direction *= -1; // reverse direction for the next column
+            }
+
+            Console.WriteLine("Done cleaning the room.");
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Map myMap = new(5, 5);
+            Map myMap = new(9, 9);
+            myMap.AddDirt(2, 3);
+            myMap.AddDirt(6, 2);
+            myMap.AddObstacle(5, 0);
 
-            // Console.WriteLine($"Grid width: {myMap.Width}");
-            // Console.WriteLine($"Grid height: {myMap.Height}");
-            myMap.AddDirt(0, 1);
-            myMap.AddObstacle(3, 0);
-            myMap.Display(1, 2);
+            Robot myRobot = new(myMap);
+            myRobot.AdjustSpeedSettings(200);
+            myRobot.StartCleaning();
         }
     }
 }
